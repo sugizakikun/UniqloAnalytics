fd92cf762fab93b30f12f0f49d09a95b83caed92
MikiSugihara0330 <sugizakikun@gmail.com>
2021-05-21 18:52:38 +0900

    [add]内容語（名詞、形容詞、副詞）を抽出する関数を追加しました（extract_content_words）

--- morphological_analysis.py
+++ morphological_analysis.py
@@ -31,28 +31,31 @@ def set_stopwords():
     return set(slothlib_stopwords);
 
 
-# 分かち書きした結果を返す関数
-# 新語を含むmecab-ipadic-neologdで形態素解析する
-tagger = MeCab.Tagger()
-
-def leaving_space_between_words_column(text):
-    splitted = ' '.join([x.split('\t')[0] for x in tagger.parse(text).splitlines()[:-1] if x.split('\t')[1].split(',')[0] not in ['助詞', '助動詞', '接続詞', '動詞', '記号']])
-    return splitted
-
-
 # リストを文字列に変換する関数
 def join_list_str(list):
     return ' '.join(list)
 
 
-# ストップワード除外関数
+# ストップワード
 stopwords = set_stopwords();
 
-def exclude_stopword(text):
-    changed_text = [token for token in text.lower().split(" ") if token != "" if token not in stopwords]
-    # 上記のままだとリスト形式になってしまうため、空白区切の文字列に変換
-    changed_text = join_list_str(changed_text)
-    return changed_text
+#内容語（名詞・形容詞・動詞・副詞）のみを抽出
+tagger = MeCab.Tagger()
+
+def extract_content_words(text):
+    content_words = [];
+    node = tagger.parseToNode(text);
+    
+    while node:
+        if( node.feature.split(",")[0] not in ['助詞', '助動詞', '接続詞', '補助記号']):
+            if( node.surface not in stopwords ):
+                content_words.append(node.surface)
+        node = node.next;
+    
+    content_words.pop(0);
+
+    return join_list_str(content_words);
+    
 
 
 review_df = pandas.read_csv('review_data.csv', index_col=0);
@@ -63,32 +66,10 @@ review_df['remove_new_line_character'] = review_df['comment'].map(remove_new_lin
 # 数字を0に置換
 review_df['comment_number_to_zero'] = review_df['remove_new_line_character'].map(replace_number_to_zero)
 
-tagger.parse('')
-node = tagger.parseToNode(review_df['comment'][0]);
+#　分かち書きしたカラムをdfに追加する  
+review_df['lsbw'] = review_df['comment_number_to_zero'].map(extract_content_words)
+print(review_df['lsbw']);
 
-while node:
-    if node.feature.split(",")[0] == "名詞":
-        print(node.surface);
-        print(node.feature);
-    elif node.feature.split(",")[0] =="動詞":
-        print(node.surface);
-        print(node.feature);
-
-    elif node.feature.split(",")[0] == "形容詞":
-        print(node.surface);
-        print(node.feature);
-
-    elif node.feature.split(",")[0] == "形容動詞":
-        print(node.surface);
-        print(node.feature);
-    else:pass
-
-    
-    node = node.next
-
-# 分かち書きしたカラムをdfに追加する
-#review_df['lsbw'] = review_df['comment_number_to_zero'].map(leaving_space_between_words_column)
-#review_df.head()
 
 #review_df['excluded'] = review_df['lsbw'].map(exclude_stopword)
 
