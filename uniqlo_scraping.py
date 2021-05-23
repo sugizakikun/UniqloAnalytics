49e35190c4bb66a1d519fe302e330fcc44f20775
MikiSugihara0330 <sugizakikun@gmail.com>
2021-05-23 21:15:32 +0900

    [fix]性別毎にエアリズムの商品コードを取得した

--- uniqlo_scraping.py
+++ uniqlo_scraping.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import review
 import pandas
 
@@ -9,31 +8,36 @@ def create_review_data_frame(reviews):
     COLUMNS = ['reviewId','productId', 'name', 'gender', 'ageRange', 'shoeSize', 'heightRange', 'weightRange', 'purchasedSize', 'rate', 'fit', 'comment' ]   
     return pandas.DataFrame(reviews, columns=COLUMNS);
 
-HEADERS = {"content-type": "application/json"}
 
-load_url = "https://www.uniqlo.com/jp/api/commerce/v5/ja/products?path=%2C%2C1568%2C3685&categoryId=1568"
-html = requests.get(load_url,  headers=HEADERS);
-data = html.json();
+CATEGORIES = [{'id' :1071, 'name'   :"WOMEN"},
+              {'id' :1072, 'name'   :"MEN"}]
 
-# 商品情報のAPI取得
-items = data['result']['items']
-
-# APIの整形
-print(json.dumps(items, indent=4));
 
 products = []; 
 
-# productsに商品名, 商品ID, 評定値, レビューした人の人数を追加
-# データの形式はsample_data.pyを参照すること
-for item in items:
-    info =  {
-                "product_name"  : item['name'],
-                "product_id"    : item['productId'],
-                "rating_score"  : item['rating']['average'],
-                "rating_count"  : item['rating']['count']
-            };
+for category in CATEGORIES: 
+    load_url = "https://www.uniqlo.com/jp/api/commerce/v5/ja/products?q=%E3%82%A8%E3%82%A2%E3%83%AA%E3%82%BA%E3%83%A0%20T%E3%82%B7%E3%83%A3%E3%83%84&path=" + str(category['id'])+ "&offset=0&limit=28"
     
-    products.append(info);
+    HEADERS = {"content-type": "application/json"}
+    html = requests.get(load_url,  headers=HEADERS);
+    data = html.json();
+    
+    # 商品情報のAPI取得
+    items = data['result']['items']
+    
+    # productsに商品名, 商品ID, 評定値, レビューした人の人数を追加
+    # データの形式はsample_data.pyを参照すること
+    for item in items:
+        if  item['genderName'] == category['name'] :
+            info =  {
+                        "product_name"  : item['name'],
+                        "product_id"    : item['productId'],
+                        "rating_score"  : item['rating']['average'],
+                        "rating_count"  : item['rating']['count'],
+                        "gender"        : item['genderName'] 
+                    };
+            
+            products.append(info);
 
 # カテゴリー内の各商品のレビューを配列格納
 review = review.get_review_data(products)
@@ -42,4 +46,4 @@ review = review.get_review_data(products)
 review_data_frame = create_review_data_frame(review)
 
 #　CSVファイルの作成
-review_data_frame.to_csv('review_data.csv', encoding='utf_8_sig');
+review_data_frame.to_csv('airism_review.csv', encoding='utf_8_sig');
